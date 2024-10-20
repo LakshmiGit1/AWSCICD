0c01e7926a3579997b8c4efa45af1675de3f400d
LakshmiGit1 <lakshmieluma@gmail.com>
2024-10-20 23:29:35 +0530

    Update start_container.sh with pull and run docker

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+echo "docker pull"
+docker pull lakshmidocker02/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+echo "docker run"
+docker run -d -p 5000:5000 lakshmidocker02/simple-python-flask-app
